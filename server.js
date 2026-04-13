0561e797cc6e67e00cff7c2ad8cd24831aea7836
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-04-13 22:57:33 +0530

    server & database setup

---
+++ server.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import app from "./src/app.js";
+import connectDB from "./src/common/config/db.js";
+
+const PORT = process.env.PORT || 8080;
+
+const start = async () => {
+  //database connection
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV} mode`);
+  });
+};
+
+start().catch((err) => {
+  console.error("Failed to start sever", err);
+  process.exit(1);
+});
---
+++ src/common/config/db.js
@@ -0,0 +1,24 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
+import * as schema from "./schema.js";
+
+const pool = new pg.Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  max: 20,
+  connectionTimeoutMillis: 5000,
+  idleTimeoutMillis: 30000,
+});
+
+export const db = drizzle(pool, { schema });
+
+const connectDB = async () => {
+  const client = await pool.connect();
+  console.log(`PostgreSQL connected: ${client.database}`);
+  client.release();
+};
+
+export default connectDB;
