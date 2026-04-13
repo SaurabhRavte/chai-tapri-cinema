0bcd7b1cd8f7714fe6c4bcde0f0a375a2f2495e6
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-04-13 23:01:34 +0530

    fixes in custom api error & reponse

--- src/common/utils/api-error.js
+++ src/common/utils/api-error.js
@@ -2,25 +2,26 @@ class ApiError extends Error {
   constructor(statusCode, message) {
     super(message);
     this.statusCode = statusCode;
-    // this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
 
   static badRequest(message = "Bad request") {
     return new ApiError(400, message);
   }
-
   static unauthorized(message = "Unauthorized") {
     return new ApiError(401, message);
+  }
+  static forbidden(message = "Forbidden") {
+    return new ApiError(403, message);
+  }
+  static notFound(message = "Not found") {
+    return new ApiError(404, message);
   }
   static conflict(message = "Conflict") {
     return new ApiError(409, message);
   }
-  static forbidden(message = "forbidden") {
-    return new ApiError(412, message);
-  }
-  static notfound(message = "notfound") {
-    return new ApiError(412, message);
+  static internal(message = "Internal server error") {
+    return new ApiError(500, message);
   }
 }
 
